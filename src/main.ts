61499a594c264d1c27fc3a70ccec0c00cdbfeddc
OmarPintado <omarpintado98@gmail.com>
2023-09-08 17:54:16 -0500

    ADD cors configuration

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+const cors = require('cors');
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 require('dotenv').config({ path: `../.env.local` });
 
@@ -17,6 +18,7 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
+  app.use(cors());
   await app.listen(3000);
 }
 bootstrap();
