b7e6f30f748c669e174e457f3f0709af19d2e9de
OmarPintado <omarpintado98@gmail.com>
2023-09-30 17:06:43 -0500

    version PROD 1.0.0

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
   const config = new DocumentBuilder()
     .setTitle('Credit Notes')
     .setDescription('API Rest related to manage credit note.')
-    .setVersion('1.0')
+    .setVersion('1.0.0')
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
